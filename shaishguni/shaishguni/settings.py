fa8746801efda6ef21aea6ddfd744efff4e4f5a4
Shaish Guni <gunisguni2007@gmail.com>
2021-03-30 01:59:06 +0545

    master

--- shaishguni/shaishguni/settings.py
+++ shaishguni/shaishguni/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '%yx&=q0we6%t716=w2k!ho777tca1d8j90xdb!t#y3w%s53(u%'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'shaishguni.herokuapp.com']
