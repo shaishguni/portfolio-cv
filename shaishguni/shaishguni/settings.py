bc2b54b9cadf211d0740885edc8eec1a6e85bf3d
Shaish Guni <gunisguni2007@gmail.com>
2021-03-30 16:49:44 +0545

    master

--- shaishguni/shaishguni/settings.py
+++ shaishguni/shaishguni/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import  os
-import django_heroku
+import django-heroku
 import dj_database_url
 from decouple  import config
 
