32dcadfcc5a18b7b874e29cb717b97b81a8e7203
Shaish Guni <gunisguni2007@gmail.com>
2021-03-30 18:49:28 +0545

    master

--- shaishguni/shaishguni/settings.py
+++ shaishguni/shaishguni/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '%yx&=q0we6%t716=w2k!ho777tca1d8j90xdb!t#y3w%s53(u%'
 # SECURITY WARNING don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['shaishguni.herokuapp.com']
+ALLOWED_HOSTS = ['shaishguni.herokuapp.com', 'mythpat.herokuapp.com']
 
 
 # Application definition
