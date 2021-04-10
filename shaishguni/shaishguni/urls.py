70ea9e14959a08a05b95d789c71f5e23d8209979
Shaish Guni <gunisguni2007@gmail.com>
2021-04-10 21:40:21 +0545

    master

--- shaishguni/shaishguni/urls.py
+++ shaishguni/shaishguni/urls.py
@@ -6,7 +6,7 @@ from django.views.static import serve
 from django.conf.urls import url
 
 
-admin.site.site_header = "Shaish Guni"
+admin.site.site_header = "shaishguni"
 admin.site.site_title = "Shaish Guni Your-Go-To-Source"
 admin.site.index_title = "Welcome to my own site."
 
