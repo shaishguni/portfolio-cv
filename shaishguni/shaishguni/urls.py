535b25a2a556731e8d508e47c3f85aa58f7b8f4a
Shaish Guni <gunisguni2007@gmail.com>
2021-04-15 19:44:54 +0545

    master

--- shaishguni/shaishguni/urls.py
+++ shaishguni/shaishguni/urls.py
@@ -15,12 +15,6 @@ urlpatterns = [
         path('admin/', admin.site.urls),
         path("", include("portfolio.urls")),
         path('admin/', admin.site.urls),
-      #    path('', include("Home.urls")),
-
-        # url(r'^media/(?P<path>.*)$',serve,
-        #     {'document_root':settings.MEDIA_ROOT}),
-        # url(r'^static/(?P<path>.*)$',serve,
-        #     {'document_root':settings.STATIC_ROOT}),
     ]
 
 
